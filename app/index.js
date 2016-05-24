3ed65c1fcc0e587731d839d283e116faffb22d70
Roy Xue <xljroy@gmail.com>
2016-05-24 11:32:01 -0700

    add hint to avoid future component missing

--- app/index.js
+++ app/index.js
@@ -39,7 +39,7 @@ module.exports = generators.NamedBase.extend({
     }, {
       type: 'input',
       name: 'userName',
-      message: 'What is your name? (Used for the package.json.)',
+      message: 'What is your name? (Used for the package.json. Capitalize the first character)',
       store: true
     }, {
       type: 'input',
